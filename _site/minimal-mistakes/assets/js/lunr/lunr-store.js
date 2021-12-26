260afb1dd77914f79b086b70a47996dc56827f4f
king-rabbit <skeptic719@gmail.com>
2021-12-26 15:56:03 +0900

    robot.txt

--- _site/minimal-mistakes/assets/js/lunr/lunr-store.js
+++ _site/minimal-mistakes/assets/js/lunr/lunr-store.js
@@ -815,9 +815,9 @@ var store = [{
         "url": "/version%20control/My-Git-Cheat-Sheet-2/",
         "teaser": null
       },{
-        "title": "Gephi를 이용해 태블로 네트워크 그래프 그리기(1)",
-        "excerpt":"안녕하세요, 킹래빗입니다. 오늘은 Gephi와 태블로를 이용해 네트워크 그래프를 그리는 법을 이야기해보려고 합니다. 약간 복잡할 수 있는데요, Gephi로 노드별 좌표값 데이터를 생성한 뒤 이를 태블로에 올린다고 생각하시면 됩니다. 여기서는 데이터를 준비하고 Gephi로 그래프를 그리는 것까지 하고, 2편에서 태블로에 그래프 생성을 완료하려고 합니다. 예시 데이터 및 데이터 정제 과정은 깃허브 리포지토리에 올려놓았으니...","categories": ["Data Visualization"],
-        "tags": ["Tableau","Gephi","Network Graph"],
-        "url": "/data%20visualization/gephi-tableau-nx-graph/",
+        "title": "test",
+        "excerpt":"테스트  ","categories": ["Version Control"],
+        "tags": ["Git"],
+        "url": "/version%20control/test/",
         "teaser": null
       }]
